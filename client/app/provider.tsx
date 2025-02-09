567f1f7424992d5c7f9bb0254eed2e8769ddff63
Prayas <prayaspal04@gmail.com>
2025-02-09 22:05:21 +0530

    final push

--- client/app/provider.tsx
+++ client/app/provider.tsx
@@ -34,7 +34,7 @@ export default function Providers({
                             privacyUrl: 'https://...',
                         },
                     }}
-                    chain={arbitrumSepolia}
+                    chain={baseSepolia}
                 >
                     {children}
                 </OnchainKitProvider>
